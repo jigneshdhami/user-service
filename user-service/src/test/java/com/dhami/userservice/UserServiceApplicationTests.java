266572c29c5513e31f2d56a7174ae23c35dcfa5a
jignesh.dhami <jignesh.dhami@cgi.com>
2021-03-30 23:51:24 +0530

    Initial commit

---
+++ user-service/src/test/java/com/dhami/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhami.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
